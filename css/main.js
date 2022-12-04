b83d2e178c57229007112df85d2b1698953ff14c
Junior94reis <99148640+Junior94reis@users.noreply.github.com>
2022-12-04 20:56:41 -0300

    primeiro commit

---
+++ css/main.js
@@ -0,0 +1,16 @@
+// function mapString(pala) {
+//     let map = { };
+//     for (let i = 0; i < pala.length; i++) {
+//       let pesq = pala[i];
+//       if (map[pesq]) {
+//         map[pesq].push(i);
+//       } else {
+//         map[pesq] = [i];
+       
+//       }
+//     }
+//      return map
+//   };
+// console.log(mapString('junior'));
+
+
